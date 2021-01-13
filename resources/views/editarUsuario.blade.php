41d2013cc7f1353410fbf9189dc4da0bfba75aa7
FernandoFdeS <52713978+FernandoFdeS@users.noreply.github.com>
2021-01-13 08:06:14 -0300

    Update editarUsuario.blade.php

--- resources/views/editarUsuario.blade.php
+++ resources/views/editarUsuario.blade.php
@@ -54,7 +54,7 @@
                 <input type="submit" class="btn btn-outline-primary mt-3 col-12">
             </div>
         </form>
-        <a href="{{route("empresa.index")}}"><button class="btn btn-outline-light text-dark mt-2  "><i class="fas fa-arrow-left"></i> Voltar</button></a>
+        <a href="{{route("paginaInicial")}}"><button class="btn btn-outline-light text-dark mt-2  "><i class="fas fa-arrow-left"></i> Voltar</button></a>
     </div>
 
     <script>
