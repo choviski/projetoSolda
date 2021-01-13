8450454d4de566cd148a04bcc6505a05c745c451
FernandoFdeS <52713978+FernandoFdeS@users.noreply.github.com>
2021-01-13 08:43:54 -0300

    Update cadastrarNovaQualificacao.blade.php

--- resources/views/cadastrarNovaQualificacao.blade.php
+++ resources/views/cadastrarNovaQualificacao.blade.php
@@ -57,7 +57,7 @@
                 <input type="submit" class="btn btn-outline-primary mt-3 col-12">
             </div>
         </form>
-        <a href="{{route("requalificacoes")}}"><button class="btn btn-outline-light mt-2 text-dark "><i class="fas fa-arrow-left"></i> Voltar</button></a>
+        <a href="{{route("paginaInicial")}}"><button class="btn btn-outline-light mt-2 text-dark "><i class="fas fa-arrow-left"></i> Voltar</button></a>
     </div>
 
 @endsection
