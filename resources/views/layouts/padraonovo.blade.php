f9e4f26ead0678918ee78b48264932aa10b1feb4
FernandoFdeS <52713978+FernandoFdeS@users.noreply.github.com>
2021-02-02 22:39:12 -0300

    Update padraonovo.blade.php

--- resources/views/layouts/padraonovo.blade.php
+++ resources/views/layouts/padraonovo.blade.php
@@ -76,7 +76,7 @@
                             </li>
                     @endif
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
+                <form class="form-inline d-flex justify-content-center my-2 my-lg-0">
                     <a href="{{route("sair")}}" class="btn btn-outline-danger my-2 my-sm-0" type="submit">Sair</a>
                 </form>
             </div>
