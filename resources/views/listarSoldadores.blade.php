de1d469fa0dcb18c8ab87078be42d018cc03419e
FernandoFdeS <fernandofdes@hotmail.com>
2021-02-05 23:12:54 -0300

    botoes editar e deletar

--- resources/views/listarSoldadores.blade.php
+++ resources/views/listarSoldadores.blade.php
@@ -17,14 +17,81 @@
             text-decoration: none;
             font-weight: normal;
         }
+        .formDelBtn{
+            position: relative;
+            transition: 0.3s ease;
+        }
+        .delBtn{
+            padding: 0px;
+            margin: 0px;
+            position: absolute;
+            font-size: 1.0rem;
+            width: 25px;
+            height: 25px;
+            top:0px;
+            right: 13px;
+            z-index: 1;
+            background-color: white;
+            color: #d92b2b;
+            font-weight: lighter;
+            border-radius: 5px;
+            transform: translateY(+50%);
+            align-items: center;
+            text-align: center;
+            transition: 0.3s ease;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #d92b2b;
+        }
+        .delBtn:hover{
+            background-color: #d92b2b;
+            color: white;
+        }
+        .delBtn:hover{
+            background-color: #d92b2b;
+        }
+        .formEditBtn{
+            position: relative;
+        }
+        .editBtn{
+            padding: 0px;
+            margin: 0px;
+            position: absolute;
+            font-size: 1.0rem;
+            width: 25px;
+            height: 25px;
+            top:0px;
+            right: 43px;
+            z-index: 1;
+            background-color: white;
+            color: #228db8;
+            font-weight: lighter;
+            border-radius: 5px;
+            transform: translateY(+50%);
+            align-items: center;
+            text-align: center;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #0c7eab;
+            transition: 0.3s ease;
+        }
+        .editBtn:hover{
+            background-color: #0c7eab;
+            color: white;
+            align-items: center;
+            text-align: center;
+        }
+
+
+
     </style>
     <div class="col-12 bg-white text-center shadow-sm rounded-bottom">
         <hr class="p-0 m-0 mb-1">
         <p class="lead p-1 m-0" style="font-size: 22px">SOLDADORES:</p>
     </div>
-    <div class="container-fluid d-flex justify-content-center flex-column col-md-9 col-sm-10 mt-3 p-0 rounded-bottom mb-2">
+    <div class="container-fluid d-flex justify-content-center flex-column col-md-9 col-sm-10 mt- p-0 rounded-bottom mb-2">
         @if($usuario->tipo==1)
-            <div id="addSoldaodre" class="col-12 mt-2 p-0 popin">
+            <div id="addSoldador" class="col-12 mt-2 p-0 popin">
                 <form method="get" action="{{route("selecionarEmpresa")}}">
                     @csrf
                     <input type="hidden" name="idEmpresa" id="idEmpresa">
@@ -34,7 +101,19 @@
         @endif
         @foreach($soldadores as $soldador)
         <!-- Aqui começa a listagem das empresas-->
-            <div id="soldadorCard" class="col-12 bg-white rounded shadow-sm d-flex justify-content-between mt-3 font-size popin">
+        <div class="warpSoldadorCard popin">
+            <div class="formDelBtn">
+                <form action="#" method="">
+                    <button type="button" class="delBtn"><i class="fas fa-times"></i></button>
+                </form>
+            </div>
+            <div class="formEditBtn">
+                <form action="#" method="">
+                    <button type="button" class="editBtn"><i class="fas fa-pen"></i></button>
+                </form>
+            </div>
+
+            <div id="soldadorCard" class="col-12 bg-white rounded shadow-sm d-flex justify-content-between mt-4 font-size">
                 <div id="infoEmpresa" class="p-2 mt-1 d-flex justify-content-end flex-column ">
                     <img id="imgSoldador" class="rounded-circle border" src="{{asset("$soldador->foto")}}" onerror="this.onerror=null;this.src='{{asset("imagens/soldador_default.png")}}';" height="125 px" width="125px">
                     <p class="nomeSoldador mt-2 border col-12">{{$soldador->nome}}</p>
@@ -47,6 +126,7 @@
                     </form>
                 </div>
             </div>
+        </div>
             <!-- Aqui acaba a listagem das empresas-->
         @endforeach
     </div>
--- resources/views/perfilSoldador.blade.php
+++ resources/views/perfilSoldador.blade.php
@@ -17,22 +17,97 @@
             text-decoration: none;
             font-weight: normal;
         }
+        .formDelBtn{
+            position: relative;
+            transition: 0.3s ease;
+        }
+        .delBtn{
+            padding: 0px;
+            margin: 0px;
+            position: absolute;
+            font-size: 1.0rem;
+            width: 25px;
+            height: 25px;
+            top:0px;
+            right: 13px;
+            z-index: 1;
+            background-color: white;
+            color: #d92b2b;
+            font-weight: lighter;
+            border-radius: 5px;
+            transform: translateY(-20%);
+            align-items: center;
+            text-align: center;
+            transition: 0.3s ease;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #d92b2b;
+        }
+        .delBtn:hover{
+            background-color: #d92b2b;
+            color: white;
+        }
+        .formEditBtn{
+            position: relative;
+        }
+        .editBtn{
+            padding: 0px;
+            margin: 0px;
+            position: absolute;
+            font-size: 1.0rem;
+            width: 25px;
+            height: 25px;
+            top:0px;
+            right: 43px;
+            z-index: 1;
+            background-color: white;
+            color: #0c7eab;
+            font-weight: lighter;
+            border-radius: 5px;
+            transform: translateY(-20%);
+            align-items: center;
+            text-align: center;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #0c7eab;
+            transition: 0.3s ease;
+
+        }
+        .editBtn:hover{
+            background-color: #0c7eab;
+            color: white;
+        }
+
+
     </style>
     <div class="container-fluid d-flex justify-content-center flex-column col-md-9 col-sm-10 mt-3 p-0 rounded-bottom ">
-        <div id="soldadorCard" class="col-12 bg-white rounded shadow-sm" >
-            <div id="soldadorInfo" class="col-12 pt-3 d-flex ">
-                <div id="imagemSoldador">
-                    <img src="{{asset($soldador->foto)}}" onerror="this.onerror=null;this.src='{{asset("imagens/soldador_default.png")}}';" class="rounded-circle border" width="150px" height="150px">
-                </div>
-                <div id="informacaoSoldador" class="d-flex flex-column p-3">
-                    <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->nome}}</p>
-                    <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->cpf}}</p>
-                    <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->matricula}}</p>
-                </div>
+        <div class="wrapSoldadorCard popin">
+            <div class="formDelBtn">
+                <form action="#" method="">
+                    <button type="button" class="delBtn"><i class="fas fa-times"></i></button>
+                </form>
             </div>
-            <div class="col-12 text-center">
-                <hr class="p-0 m-0 mb-1">
-                <p class="lead p-1 m-0" style="font-size: 22px">QUALIFICAÇÕES:</p>
+            <div class="formEditBtn">
+                <form action="#" method="">
+                    <button type="button" class="editBtn"><i class="fas fa-pen"></i></button>
+                </form>
+            </div>
+
+            <div id="soldadorCard" class="col-12 bg-white rounded shadow-sm mt-2" >
+                <div id="soldadorInfo" class="col-12 pt-3 d-flex ">
+                    <div id="imagemSoldador">
+                        <img src="{{asset($soldador->foto)}}" onerror="this.onerror=null;this.src='{{asset("imagens/soldador_default.png")}}';" class="rounded-circle border" width="150px" height="150px">
+                    </div>
+                    <div id="informacaoSoldador" class="d-flex flex-column p-3">
+                        <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->nome}}</p>
+                        <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->cpf}}</p>
+                        <p class="p-0 font-weight-light" style="font-size: 22px">{{$soldador->matricula}}</p>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    <hr class="p-0 m-0 mb-1">
+                    <p class="lead p-1 m-0" style="font-size: 22px">QUALIFICAÇÕES:</p>
+                </div>
             </div>
         </div>
         @if($usuario->tipo==1)
