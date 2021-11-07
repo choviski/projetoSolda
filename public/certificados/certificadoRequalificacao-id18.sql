fad0d0c8553f05670d7107e72b342301cc2e6ad5
choviski <wojeicchowskinicholas@gmail.com>
2021-11-06 23:24:35 -0300

    funcionalidade de selecionar eps2

--- public/certificados/certificadoRequalificacao-id18.sql
+++
@@ -1,4 +0,0 @@
-select P.name as "nome do produto", A.id as "id do armazém", 
-I.amount_in_stock as "estoque atual", I.reorder_point as "ponto de ressuprimento" 
-from s_product as P, s_warehouse as A, s_inventory as I
-where(I.amount_in_stock<I.reorder_point and I.product_id=P.id and I.warehouse_id = A.id);
