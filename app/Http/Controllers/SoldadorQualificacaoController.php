d7ffa34542700743d817404a1d8b01bc3db843d9
FernandoFdeS <fernandofdes@hotmail.com>
2024-03-14 17:44:46 -0300

    Corrige nome de variável

--- app/Http/Controllers/SoldadorQualificacaoController.php
+++ app/Http/Controllers/SoldadorQualificacaoController.php
@@ -71,7 +71,7 @@ class SoldadorQualificacaoController extends Controller
         #SoldadorQualificacao::destroy($request->id);
         $empresas = Empresa::orderBy("razao_social")->paginate(10);
         $usuario=session()->get("Usuario");
-        return view("listarEmpresas")->with(["usuario"=>$usuario,"termo"=>$termo,"soldador"=>$empresas]);
+        return view("listarEmpresas")->with(["usuario"=>$usuario,"termo"=>$termo,"empresas"=>$empresas]);
 
     }
     public function editar($id)
