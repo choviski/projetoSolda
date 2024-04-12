0b98b74e7fba07eb252f7f5011e741eccc24a8df
FernandoFdeS <fernandofdes@hotmail.com>
2024-04-12 16:51:51 -0300

    Descomenta linha que apaga qualificacaokkkkk

--- app/Http/Controllers/SoldadorQualificacaoController.php
+++ app/Http/Controllers/SoldadorQualificacaoController.php
@@ -69,7 +69,7 @@ class SoldadorQualificacaoController extends Controller
     public function destroy(Request $request)
     {
         $termo = $request->filtro;
-        #SoldadorQualificacao::destroy($request->id);
+        $SoldadorQualificacao::destroy($request->id);
         $empresas = Empresa::orderBy("razao_social")->paginate(10);
         $usuario=session()->get("Usuario");
         return view("listarEmpresas")->with(["usuario"=>$usuario,"termo"=>$termo,"empresas"=>$empresas]);
